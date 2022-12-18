9836a79abac956d15a7457306d8bdff891d263e1
Guy <guykeren2002@gmail.com>
2022-12-18 07:19:20 +0200

    l

---
+++ Project-DataBase/DAL/UserServiceDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Project_DataBase.DAL
+{
+    public class UserServiceDAL
+    {
+        public static GetId(string username,string password)
+        {
+            string query = $"exec [GetUserID] {username},{passsword}";
+            return SQLHelper.SelectScalarToInt32(query);
+
+                }
+    }
+}
---
+++ Project-DataBase/web_service/WebService1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebService1.asmx.cs" Class="Project_DataBase.web_service.WebService1" %>
